b3dd0a78676364c487d82ba780881d85b63d780e
Dhalendra <dhalendra.chandrawanshi@gmail.com>
2016-12-19 21:20:40 -0500

    Add files via upload

---
+++ HelloSpring/src/com/dc/learn/MainApp.java
@@ -0,0 +1,31 @@
+package com.dc.learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+	      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+
+	      objA.getMessage1();
+	      objA.getMessage2();
+
+	      HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
+	      objB.getMessage1();
+	      objB.getMessage2();
+	      objB.getMessage3();
+	      
+	      context.registerShutdownHook();
+	   }
+
+	}
+
+
